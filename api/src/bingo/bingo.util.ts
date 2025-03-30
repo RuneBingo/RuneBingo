d9de9ab3c72c4029722fe7f521da5e54e93918dd
Raphaël Pion <raphael.pion@outlook.com>
2025-03-30 16:16:41 -0400

    [Review] Add todo to slugify call

--- api/src/bingo/bingo.util.ts
+++ api/src/bingo/bingo.util.ts
@@ -1,6 +1,8 @@
 import slugify from 'slugify';
 
-export function slugifyTitle(title: string) {
+export function slugifyTitle(title: string): string {
+  // TODO: find out why this is unsafe and fix it
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
   return slugify(title, {
     lower: true,
     strict: true,
